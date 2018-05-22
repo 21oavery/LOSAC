aacc939dac5eb48e1cb08a9a62ad44bc4a20693f
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2018-05-22 11:33:59 -0400

    Update solve.c

--- solve.c
+++ solve.c
@@ -6,20 +6,23 @@
 #define LOSAC_CONTAINS 4
 #define LOSAC_NCONTAINS 5
 
-#define LOSAC_ADD
-#define LOSAC_SUB
-#define LOSAC_MUL
-#define LOSAC_DIV
+#define LOSAC_ADD 0
+#define LOSAC_SUB 1
+#define LOSAC_MUL 2
+#define LOSAC_DIV 3
+#define LOSAC_MOD 4
 
 struct losac_statement {
     unsigned long p1;
     union {
         unsigned long p2;
         unsigned long p3;
+        double d;
+    }
     unsigned char type;
     unsigned char op;
 }
 
-/* If A = B, replace A with B
+// If A = B, replace A with B
 int iterateRemoveEq(struct losac_statement *data, unsigned long preserve, unsigned int length) {
     for 
