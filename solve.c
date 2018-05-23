b64973a27b4f7f9e36519d9a10fd23d7a90dbcce
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2018-05-22 21:33:49 -0400

    Added detectFault()

--- solve.c
+++ solve.c
@@ -1,28 +1,79 @@
-#define LOSAC_EQ 0
-#define LOSAC_NEQ 1
-#define LOSAC_LESS 2
-#define LOSAC_LESSEQ 3
+#define LOSAC_TNULL 0
+#define LOSAC_EQ 1
+#define LOSAC_NEQ 2
+#define LOSAC_LESS 3
+#define LOSAC_LESSEQ 4
 
 #define LOSAC_CONTAINS 4
 #define LOSAC_NCONTAINS 5
 
-#define LOSAC_ADD 0
-#define LOSAC_SUB 1
-#define LOSAC_MUL 2
-#define LOSAC_DIV 3
-#define LOSAC_MOD 4
+#define LOSAC_OPNONE 0
+#define LOSAC_ADD 1
+#define LOSAC_SUB 2
+#define LOSAC_MUL 3
+#define LOSAC_DIV 4
+#define LOSAC_EXP 5
+#define LOSAC_MOD 6
+
+#define LOSAC_CONST 7
+#define LOSAC_CONSTD 8
+#define LOSAC_IMP 9
+
+#define losac_opHasVarP2(x) ((x < 7) && (x != LOSAC_OPNONE))
+#define losac_opHasVarP3(x) losac_opHasVarP2(x)
+
+#define LOSAC_CALC 0
+#define LOSAC_FUNC 1
+#define LOSAC_FORM 2
 
 struct losac_statement {
-    unsigned long p1;
     union {
-        unsigned long p2;
-        unsigned long p3;
+        struct {
+            unsigned long p2;
+            unsigned long p3;
+        } p;
         double d;
-    }
+    } pd;
+    unsigned long p1;
     unsigned char type;
     unsigned char op;
 }
 
-// If A = B, replace A with B
-int iterateRemoveEq(struct losac_statement *data, unsigned long preserve, unsigned int length) {
-    for 
+struct losac_task {
+    struct losac_statement *data;
+    unsigned long s;
+    unsigned long sMem;
+    unsigned long target; // Only used for calculations, sets target
+    unsigned char t;
+}
+
+// If A = B where both are variables, replace A with B
+void removeVarEq(struct losac_task *in) {
+    while (true) {
+        unsigned long i;
+        for (i = 0; i < in->s; i++) if ((in->data[i].t == LOSAC_EQ) && (in->data[i].op == LOSAC_OPNONE)) break;
+        if (i == in->s) return;
+        unsigned long f = in->data[i].p1;
+        unsigned long r = in->data[i].pd.p.p2;
+        for (unsigned long j = 0; j < in->s; j++) {
+            if (in->data[j].p1 == f) in->data[j].p1 = r;
+            if (losac_opHasP2(in->data[j].op) && (in->data[j].pd.p.p2 == f)) in->data[j].pd.p.p2 = r;
+            if (losac_opHasP3(in->data[j].op) && (in->data[j].pd.p.p3 == f)) in->data[j].pd.p.p3 = r;
+        }
+    }
+}
+
+// Detects A != A, A < A, A does not contain A
+unsigned long detectFault(struct losac_task *in) {
+    for (unsigned long i = 0; i < in->s; i++) {
+        if ((in->data[i].op == LOSAC_OPNONE) && (in->data[i].p1 == in->data[i].pd.p.p2)) {
+            switch (in->data[i].type) {
+                case LOSAC_NEQ:
+                case LOSAC_LESS:
+                case LOSAC_NCONTAINS:
+                    return i;
+            }
+        }
+    }
+    return in->s;
+}
